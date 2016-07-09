2247a57353355bbc86cb04d9961a8781a65d715e
ryanseaton568 <ryanseaton568@outlook.com>
2016-07-09 11:03:10 -0500

    Daily4. Read Description
    My code can be found in:
    Daily4\Form1.vb

---
+++ Daily4/Daily4/Form1.Designer.vb
@@ -0,0 +1,62 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frmDaily4
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.cmbGame = New System.Windows.Forms.ComboBox()
+        Me.btnDisplayCounts = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'cmbGame
+        '
+        Me.cmbGame.FormattingEnabled = True
+        Me.cmbGame.Items.AddRange(New Object() {"Daily4Morning.csv", "Daily4Day.csv", "Daily4Evening.csv", "Daily4Night.csv"})
+        Me.cmbGame.Location = New System.Drawing.Point(83, 23)
+        Me.cmbGame.Name = "cmbGame"
+        Me.cmbGame.Size = New System.Drawing.Size(121, 21)
+        Me.cmbGame.TabIndex = 0
+        '
+        'btnDisplayCounts
+        '
+        Me.btnDisplayCounts.Location = New System.Drawing.Point(103, 64)
+        Me.btnDisplayCounts.Name = "btnDisplayCounts"
+        Me.btnDisplayCounts.Size = New System.Drawing.Size(80, 43)
+        Me.btnDisplayCounts.TabIndex = 1
+        Me.btnDisplayCounts.Text = "Display Counts"
+        Me.btnDisplayCounts.UseVisualStyleBackColor = True
+        '
+        'frmDaily4
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(293, 132)
+        Me.Controls.Add(Me.btnDisplayCounts)
+        Me.Controls.Add(Me.cmbGame)
+        Me.Name = "frmDaily4"
+        Me.Text = "Daily 4 - Joseph Seaton"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents cmbGame As ComboBox
+    Friend WithEvents btnDisplayCounts As Button
+End Class
---
+++ Daily4/Daily4/Form1.vb
@@ -0,0 +1,37 @@
+﻿Option Strict On
+Imports System.IO
+Public Class frmDaily4
+    Dim gintCounts(,), gintSums() As Integer
+
+    Private Sub cmbGame_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cmbGame.SelectedIndexChanged
+        ReDim gintCounts(9, 3), gintSums(36)
+        Dim fsrDaily4 = New StreamReader(cmbGame.Text)
+        Dim strDailyFour(8) As String
+        Dim intColumn As Integer
+        Do Until fsrDaily4.EndOfStream
+            strDailyFour = Split(fsrDaily4.ReadLine, (","))
+            For intColumn = 4 To 7
+                gintCounts(CInt(strDailyFour(intColumn)), intColumn - 4) += 1
+            Next intColumn
+            gintSums(CInt(strDailyFour(8))) += 1
+        Loop
+        fsrDaily4.Close()
+    End Sub
+
+    Private Sub btnDisplayCounts_Click(sender As Object, e As EventArgs) Handles btnDisplayCounts.Click
+        Dim intRow, intColumn As Integer
+        Console.WriteLine(StrDup(31, "-"))
+        Console.WriteLine(cmbGame.Text)
+        Console.WriteLine(StrDup(31, "-"))
+        For intRow = 0 To 9
+            Console.Write("{0,3} - ", intRow)
+            For intColumn = 0 To 3
+                Console.Write("{0,5}", gintCounts(intRow, intColumn))
+            Next intColumn
+            Console.WriteLine("{0,5}", gintSums(intRow))
+        Next intRow
+        For intRow = 10 To 36
+            Console.WriteLine("{0,3}{1,2}{2,26}", intRow, "-", gintSums(intRow))
+        Next
+    End Sub
+End Class
