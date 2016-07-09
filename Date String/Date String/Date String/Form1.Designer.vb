09b9ad6b8660832bdd1af89cb0769f4e83fda4f6
ryanseaton568 <ryanseaton568@outlook.com>
2016-07-09 11:05:34 -0500

    Date String. Read Description
    My code can be found:
    Date String\Date String\Form1.vb

---
+++ Date String/Date String/Date String/Form1.Designer.vb
@@ -0,0 +1,179 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmDateString
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtDayOfWeek = New System.Windows.Forms.TextBox()
+        Me.txtMonth = New System.Windows.Forms.TextBox()
+        Me.txtDayOfMonth = New System.Windows.Forms.TextBox()
+        Me.txtYear = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.btnShowDate = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.lblDateString = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'txtDayOfWeek
+        '
+        Me.txtDayOfWeek.Location = New System.Drawing.Point(172, 71)
+        Me.txtDayOfWeek.Name = "txtDayOfWeek"
+        Me.txtDayOfWeek.Size = New System.Drawing.Size(100, 20)
+        Me.txtDayOfWeek.TabIndex = 2
+        '
+        'txtMonth
+        '
+        Me.txtMonth.Location = New System.Drawing.Point(172, 19)
+        Me.txtMonth.Name = "txtMonth"
+        Me.txtMonth.Size = New System.Drawing.Size(100, 20)
+        Me.txtMonth.TabIndex = 0
+        '
+        'txtDayOfMonth
+        '
+        Me.txtDayOfMonth.Location = New System.Drawing.Point(172, 45)
+        Me.txtDayOfMonth.Name = "txtDayOfMonth"
+        Me.txtDayOfMonth.Size = New System.Drawing.Size(100, 20)
+        Me.txtDayOfMonth.TabIndex = 1
+        '
+        'txtYear
+        '
+        Me.txtYear.Location = New System.Drawing.Point(172, 97)
+        Me.txtYear.Name = "txtYear"
+        Me.txtYear.Size = New System.Drawing.Size(100, 20)
+        Me.txtYear.TabIndex = 3
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(37, 74)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(129, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Enter the day of the week"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(84, 22)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(82, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Enter the month"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(34, 48)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(132, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Enter the day of the month"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(93, 100)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(73, 13)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Enter the year"
+        '
+        'btnShowDate
+        '
+        Me.btnShowDate.Location = New System.Drawing.Point(26, 168)
+        Me.btnShowDate.Name = "btnShowDate"
+        Me.btnShowDate.Size = New System.Drawing.Size(75, 23)
+        Me.btnShowDate.TabIndex = 4
+        Me.btnShowDate.Text = "Show &Date"
+        Me.btnShowDate.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(107, 168)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(75, 23)
+        Me.btnClear.TabIndex = 5
+        Me.btnClear.Text = "Clea&r"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnExit.Location = New System.Drawing.Point(188, 168)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 6
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'lblDateString
+        '
+        Me.lblDateString.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblDateString.Location = New System.Drawing.Point(12, 132)
+        Me.lblDateString.Name = "lblDateString"
+        Me.lblDateString.Size = New System.Drawing.Size(260, 23)
+        Me.lblDateString.TabIndex = 8
+        Me.lblDateString.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'frmDateString
+        '
+        Me.AcceptButton = Me.btnShowDate
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CancelButton = Me.btnExit
+        Me.ClientSize = New System.Drawing.Size(284, 205)
+        Me.Controls.Add(Me.lblDateString)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnShowDate)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtYear)
+        Me.Controls.Add(Me.txtDayOfMonth)
+        Me.Controls.Add(Me.txtMonth)
+        Me.Controls.Add(Me.txtDayOfWeek)
+        Me.Name = "frmDateString"
+        Me.ShowIcon = False
+        Me.Text = "Date String"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtDayOfWeek As TextBox
+    Friend WithEvents txtMonth As TextBox
+    Friend WithEvents txtDayOfMonth As TextBox
+    Friend WithEvents txtYear As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents btnShowDate As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents lblDateString As Label
+End Class
---
+++ Date String/Date String/Date String/Form1.vb
@@ -0,0 +1,26 @@
+﻿Public Class frmDateString
+    Private Sub btnShowDate_Click(sender As Object, e As EventArgs) Handles btnShowDate.Click
+        ' Concatenate the input and build the date string.
+        lblDateString.Text = txtDayOfWeek.Text & ", " &
+            txtMonth.Text & " " &
+            txtDayOfMonth.Text & ", " &
+            txtYear.Text
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        'Clear the Text Boxes and lblDateString.
+        txtDayOfWeek.Clear()
+        txtMonth.Clear()
+        txtDayOfMonth.Clear()
+        txtYear.Clear()
+        lblDateString.Text = String.Empty
+
+        ' Give the focus to txtMonth.
+        txtMonth.Focus()
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        ' Close the form.
+        Me.Close()
+    End Sub
+End Class
