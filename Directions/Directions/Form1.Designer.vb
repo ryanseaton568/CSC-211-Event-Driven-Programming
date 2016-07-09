160965e7c2774e0e3af9d7b9bac7005b27344208
ryanseaton568 <ryanseaton568@outlook.com>
2016-07-09 11:08:41 -0500

    Directions. Read Description.
    Code can be found in:
    \Directions\Form1.vb

---
+++ Directions/Directions/Form1.Designer.vb
@@ -0,0 +1,110 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmDirections
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmDirections))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.lblDirections = New System.Windows.Forms.Label()
+        Me.btnDisplayDirections = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.SystemColors.ControlDarkDark
+        Me.Label1.Location = New System.Drawing.Point(42, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(368, 25)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Directions to the Highlander Hotel"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(101, 37)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(232, 213)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox1.TabIndex = 1
+        Me.PictureBox1.TabStop = False
+        '
+        'lblDirections
+        '
+        Me.lblDirections.AutoSize = True
+        Me.lblDirections.Location = New System.Drawing.Point(77, 263)
+        Me.lblDirections.Name = "lblDirections"
+        Me.lblDirections.Size = New System.Drawing.Size(300, 39)
+        Me.lblDirections.TabIndex = 2
+        Me.lblDirections.Text = "Traveling on I-89, take Exit 125 South. The hotel is on the left." & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Traveling on H" &
+    "ighway 101 North, the hotel is on the right," & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "just before the I-89 intersection." &
+    ""
+        Me.lblDirections.Visible = False
+        '
+        'btnDisplayDirections
+        '
+        Me.btnDisplayDirections.Location = New System.Drawing.Point(101, 317)
+        Me.btnDisplayDirections.Name = "btnDisplayDirections"
+        Me.btnDisplayDirections.Size = New System.Drawing.Size(74, 36)
+        Me.btnDisplayDirections.TabIndex = 3
+        Me.btnDisplayDirections.Text = "Display DIrections"
+        Me.btnDisplayDirections.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(258, 317)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 36)
+        Me.btnExit.TabIndex = 4
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'frmDirections
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(441, 380)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnDisplayDirections)
+        Me.Controls.Add(Me.lblDirections)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Name = "frmDirections"
+        Me.Text = "Directions"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents lblDirections As Label
+    Friend WithEvents btnDisplayDirections As Button
+    Friend WithEvents btnExit As Button
+End Class
---
+++ Directions/Directions/Form1.vb
@@ -0,0 +1,17 @@
+﻿Option Strict On
+Public Class frmDirections
+    Private Sub btnDisplayDirections_Click(sender As Object, e As EventArgs) Handles btnDisplayDirections.Click
+        ' Make the directions visible.
+        lblDirections.Visible = Not lblDirections.Visible
+
+        If lblDirections.Visible Then
+            btnDisplayDirections.Text = "Hide Directions"
+        Else
+            btnDisplayDirections.Text = "Display Directions"
+        End If
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+End Class
